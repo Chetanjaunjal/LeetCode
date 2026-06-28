7608f8aebcac3ef67558a810599067b94587faa5
Chetanjaunjal <chetanjaunjal1@gmail.com>
2026-06-28 21:01:33 +0530

    Time: 3 ms (71.82%), Space: 48 MB (64.74%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int[] findDiagonalOrder(int[][] arr) {
+        // if (arr == null || arr.length == 0) return new int[0];
+
+        int m = arr.length, n = arr[0].length;
+        int[] ans = new int[m * n];
+        int row = 0, col = 0;
+
+        for (int i = 0; i < m * n; i++) {
+            ans[i] = arr[row][col];
+
+            if ((row + col) % 2 == 0) {
+                if (col == n - 1) row++;
+                else if (row == 0) col++;
+                else { row--; col++; }
+            } else {
+                if (row == m - 1) col++;
+                else if (col == 0) row++;
+                else { row++; col--; }
+            }
+        }
+
+        return ans;
+    }
+}
