016dd46ef9320e0aa362e76bd8050968448eee61
Chetanjaunjal <chetanjaunjal1@gmail.com>
2026-07-01 08:17:38 +0530

    Time: 0 ms (100%), Space: 43.7 MB (10.59%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,13 +1,30 @@
 class Solution {
     public void sortColors(int[] nums) {
-       for(int i = 0; i < nums.length - 1; i++){
-    for(int j = 0; j < nums.length - i - 1; j++){
-        if(nums[j] > nums[j+1]){
-            int temp = nums[j];
-            nums[j] = nums[j+1];
-            nums[j+1] = temp;
+
+        int n = nums.length;
+
+        int[] zero = new int[n];
+        int[] one = new int[n];
+        int[] two = new int[n];
+        int a = 0, b = 0, c = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                zero[a++] = 0;
+            } else if (nums[i] == 1) {
+                one[b++] = 1;
+            } else {
+                two[c++] = 2;
+            }
         }
-    }
-}
+        int index = 0;
+        for (int i = 0; i < a; i++) {
+            nums[index++] = zero[i];
+        }
+        for (int i = 0; i < b; i++) {
+            nums[index++] = one[i];
+        }
+        for (int i = 0; i < c; i++) {
+            nums[index++] = two[i];
+        }
     }
 }
