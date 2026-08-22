60244925369b322b9b54a6526d2eba79785db8ef
Chetanjaunjal <chetanjaunjal1@gmail.com>
2026-08-22 15:06:08 +0530

    Time: 0 ms (100%), Space: 42.3 MB (62.34%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int sum = 0;
+        int product = 1;
+        for(int x = n; x > 0; x/=10){
+            int r = x % 10;
+            sum += r;
+            product *= r;
+        }
+        return n%(sum+product) == 0;
+    }
+}
