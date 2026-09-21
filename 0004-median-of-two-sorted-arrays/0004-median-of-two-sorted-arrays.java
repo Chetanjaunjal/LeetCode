2ace605f9b7d07983f099ecb232029ac46b50287
Chetanjaunjal <chetanjaunjal1@gmail.com>
2026-09-21 22:23:40 +0530

    Time: 1 ms (100%), Space: 48.9 MB (32.34%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,30 +2,44 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
-        int[] mergeArray = new int[n+m];
-        int i = 0, j = 0, k = 0;
-        while(i < n && j < m){
-            if (nums1[i] <= nums2[j]){
-                mergeArray[k++] = nums1[i++];
-            } else {
-                mergeArray[k++] = nums2[j++];
+        if (n > m) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int low = 0;
+        int high = n;
+        int left = (n + m + 1) / 2;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+            if (mid1 < n) {
+                r1 = nums1[mid1];
+            }
+            if (mid2 < m) {
+                r2 = nums2[mid2];
+            }
+            if (mid1 - 1 >= 0) {
+                l1 = nums1[mid1 - 1];
+            }
+            if (mid2 - 1 >= 0) {
+                l2 = nums2[mid2 - 1];
+            }
+            if (l1 <= r2 && l2 <= r1) {
+                if ((n + m) % 2 == 1) {
+                    return Math.max(l1, l2);
+                }
+                return ((double) Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+            }
+            else if (l1 > r2) {
+                high = mid1 - 1;
+            }
+            else {
+                low = mid1 + 1;
             }
         }
-             while (i < n) {
-            mergeArray[k++] = nums1[i++];
-        }
-        while (j < m) {
-            mergeArray[k++] = nums2[j++];
-        }
-        int z = mergeArray.length;
-        float median = 0;
-        //int idx1 = z/2;
-        int idx2 = (z/2)-1;
-            if(z%2 == 0){
-                median = (mergeArray[z/2] + mergeArray[idx2])/2.0f;
-            } else {
-                median = mergeArray[z/2];
-            }
-            return median;
+        return 0.0;
     }
 }
